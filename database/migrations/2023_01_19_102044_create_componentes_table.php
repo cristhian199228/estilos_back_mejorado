51648b539b4f1081d1675319341ee5f5ba119303
Your Name <you@example.com>
2023-01-20 20:33:50 -0500

    terminado

---
+++ database/migrations/2023_01_19_102044_create_componentes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('componentes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('url');
+            $table->foreignId('forma_id')->constrained();
+            $table->foreignId('plantilla_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('componentes');
+    }
+};
